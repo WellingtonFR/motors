efe0f1c8ad9553b7b1f38f3ee28cc3e3e2740417
WellingtonFR <wellingtondfr@gmail.com>
2024-08-28 22:33:01 -0300

    Adiciona a função para requisição da API e a interface com os valores e tipos retornados

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { Submenu } from "./components/home/submenu";
+import { HomeProps } from "./utils/home.type";
 
-export default function Home() {
+import { getDataHome } from "./utils/actions/get-data";
+
+export default async function Home() {
+  const { object }: HomeProps = await getDataHome();
+  console.log(object.title);
+
   return (
     <main>
       <Submenu />
---
+++ src/app/utils/actions/get-data.ts
@@ -0,0 +1,15 @@
+export async function getDataHome() {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/objects/66cf723b23887ff69c06f607?read_key=${process.env.READ_KEY}&depth=1&props=slug,title,metadata`
+    );
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    return res.json();
+  } catch (err) {
+    throw new Error("Failed to fetch data");
+  }
+}
