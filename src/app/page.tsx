c62aa3489c3d993f4aab7d8e343b1dcdb5d2f4ad
WellingtonFR <wellingtondfr@gmail.com>
2024-08-28 22:54:16 -0300

    adiciona o hero da página inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import { Submenu } from "./components/home/submenu";
 import { HomeProps } from "./utils/home.type";
 
 import { getDataHome } from "./utils/actions/get-data";
+import { Hero } from "./components/hero";
+import { Phone } from "lucide-react";
 
 export default async function Home() {
   const { object }: HomeProps = await getDataHome();
@@ -12,6 +14,13 @@ export default async function Home() {
   return (
     <main>
       <Submenu />
+      <Hero
+        heading={object.metadata.heading}
+        buttonTitle={object.metadata.cta_button.title}
+        buttonUrl={object.metadata.cta_button.url}
+        bannerUrl={object.metadata.banner.url}
+        icon={<Phone size={24} color="#FFF" />}
+      />
     </main>
   );
 }
